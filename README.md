42f4fbfa44a7b00bd6cf35f25fbb68e62f33fd97
jayson-leach <jleach@ucsd.edu>
2022-02-02 11:19:10 -0800

    initialization

---
+++ README.md
@@ -0,0 +1,70 @@
+<!-- NFT Fractionalizaton Demo -->
+## NFT Fractionalization Demo
+
+This project demonstrates the basic idea behind NFT fractionalization, based on conversations that took place during meetings in Weeks 2 and 3.
+
+
+
+### Built With
+
+* [Hardhat](https://hardhat.org/getting-started/)
+
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+To get a local copy up and running follow these simple example steps.
+
+### Prerequisites
+
+This demo uses Hardhat tools in order to run a blockchain simulation on hardware.
+
+* npm
+  ```sh
+  npm install npm@latest -g
+  ```
+
+### Installation
+
+_Below is an example of how you can instruct your audience on installing and setting up your app. This template doesn't rely on any external dependencies or services._
+
+1. Get a free API Key at [https://example.com](https://example.com)
+2. Clone the repo
+   ```sh
+   git clone https://github.com/your_username_/Project-Name.git
+   ```
+3. Install NPM packages
+   ```sh
+   npm install
+   ```
+4. Enter your API in `config.js`
+   ```js
+   const API_KEY = 'ENTER YOUR API';
+   ```
+
+
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+Much of the code and direction for this demo is taken directly from a project on Buildspace, specifically the project titled "Mint your own NFT collection and ship a Web3 app to show them off."
+
+* Their Website (https://buildspace.so)
+* Their Smart Contract (https://gist.github.com/farzaa/b3b8ec8aded7e5876b8a1ab786347cc9)
+
+
+
+<!-- CONTACT -->
+## Contact
+
+Jayson Leach - jleach@ucsd.edu
+
+Project Link: [https://github.com/your_username/repo_name](https://github.com/your_username/repo_name)
+
+
+
+Visual Studio Code -
+The main software used to develop this demo is VS Code. 
+More info on VS Code: https://code.visualstudio.com/docs
+
---
+++ contracts/FractTest.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "hardhat/console.sol";
+import { Base64 } from "./libraries/Base64.sol";
+
+contract FractNFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    
+    constructor() ERC721 ("FractNFT", "FRACT") {
+        console.log("Creates NFT. Can parse other NFT of the same type and accrue their data.");
+    }
+
+    // Create New NFT, thus no nests or other data to grab from
+    function mintNewFractNFT(string memory set_name, string memory data) public {
+        // Get the current tokenId, this starts at 0.
+        uint256 newItemId = _tokenIds.current();
+
+        // Package all the JSON metadata
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        set_name,
+                        '", "creator": "',
+                        msg.sender,
+                        '", "nests": "", "data": "',
+                        data,'"}'
+                    )
+                )
+            )
+        );
+
+        // Base64 encode JSON metadata
+        string memory finalTokenUri = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+
+        console.log("\n--------------------");
+        console.log(finalTokenUri);
+        console.log("--------------------\n");
+
+        _safeMint(msg.sender, newItemId);
+        
+        // Update URI
+        _setTokenURI(newItemId, finalTokenUri);
+    
+        _tokenIds.increment();
+        console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+    }
+
+    // Create NFT from other NFT (one or more) of the same type
+    function mintFractNFT(string memory set_name, string memory nests, string memory data) public {
+        // Get the current tokenId, this starts at 0.
+        uint256 newItemId = _tokenIds.current();
+
+        // Package all the JSON metadata
+        string memory json = Base64.encode(
+            bytes(
+                string(
+                    abi.encodePacked(
+                        '{"name": "',
+                        set_name,
+                        '", "creator": "',
+                        msg.sender,
+                        '", "nests": "',
+                        nests,
+                        '", "data": "',
+                        data,'"}'
+                    )
+                )
+            )
+        );
+
+        // Base64 encode JSON metadata
+        string memory finalTokenUri = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+
+        console.log("\n--------------------");
+        console.log(finalTokenUri);
+        console.log("--------------------\n");
+
+        _safeMint(msg.sender, newItemId);
+        
+        // Update URI
+        _setTokenURI(newItemId, finalTokenUri);
+    
+        _tokenIds.increment();
+        console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+    }
+}
